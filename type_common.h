b6b0c481958ffb2894279305000489286648e0f3
sohee_jung <kastro1030@dgist.ac.kr>
2024-11-25 20:02:36 +0900

    type_common.h 추가 구현 전체에서 쓸 common type에 대한 정의용 header file 추가

---
+++ type_common.h
@@ -0,0 +1,14 @@
+#ifndef TYPE_COMMOH_
+#define TYPE_COMMON
+
+#include "stdint.h"
+
+// ID
+typedef int8_t bank_id_t;      // bank ID
+typedef int32_t card_id_t;     // card ID
+typedef int32_t account_id_t;  // account ID
+
+// PASSWORD
+typedef uint64_t card_pin_t;         //
+typedef uint64_t account_password_t; //
+#endif
