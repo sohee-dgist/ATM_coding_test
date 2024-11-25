e0c066eea96647daa8490f2680368498584b4ba8
sohee_jung <kastro1030@dgist.ac.kr>
2024-11-25 22:47:10 +0900

    card 생성 부분 오류 해결 오류해결 ifdef로 되어 있던것 ifndef로 변경

--- card/card.h
+++ card/card.h
@@ -1,14 +1,15 @@
-#ifdef CARD_H_
+#ifndef CARD_H_
 #define CARD_H_
 
 #include "type_common.h"
 
-namespace card
+namespace card {
 class Card {
 public:
+  Card(card_id_t card_id) : card_id_(card_id) {}
   card_id_t get_card_id() { return card_id_; }
 private:
   card_id_t card_id_;
-}
-
+};
+} // namespace card
 #endif
