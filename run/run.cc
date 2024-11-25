df50ccc13b8ca84d825abfba1823d5c12619b74b
sohee_jung <kastro1030@dgist.ac.kr>
2024-11-25 20:17:33 +0900

    run.cc 파일 (= main 파일) 추가 구현 간단한 입출금 조회 시나리오 테스트 용도

---
+++ run/run.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Running Here" << std::endl;
+    // Bank 만들고
+
+    // ATM 만들고
+
+    // Card 만들고 (INSERT는 강제로 시킴) <-- 꽂았다고 가정할 것
+
+    // Session 무한으로 열었다 닫았다하기
+}
